8ae4f52e44d059a48f01d612aae3233c3b95fcb2
Abdulbaki Dursun <eggsydev@gmail.com>
2021-09-23 23:40:31 +0300

    refactor: use the new component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,8 @@ import { useMemo, useState } from "react";
 
 import AnimatedBox from "../components/AnimatedBox";
 import AnimatedCheckBox from "../components/AnimatedCheckbox";
-import Button from "../components/Button";
+import Button from "../components/Button/Default";
+import SoundButton from "../components/Button/Sound";
 import Confetti from "../components/Confetti";
 import TrollFace from "../components/TrollFace";
 import Sayings from "../components/Sayings";
@@ -25,6 +26,7 @@ interface Data {
 const Home: NextPage = () => {
   const [currentStrings, setCurrentStrings] = useState([strings[0]]);
   const [forceReverseResult, setForceReverseResult] = useState(false);
+  const [soundEnabled, setSoundEnabled] = useState(true);
   const [isHiddenButton, setIsHiddenButton] = useState(0);
 
   /* Fetch data */
@@ -208,25 +210,10 @@ const Home: NextPage = () => {
           className="flex flex-wrap items-center gap-2"
           onClick={() => setIsHiddenButton((prev) => prev + 1)}
         >
-          <div
-            className="px-4 py-2 text-gray-400 rounded-lg bg-gray-50"
-            title="Oynatılacak müziğe dikkat edin."
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-6 h-6 animate-pulse"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M15.536 8.464a5 5 0 010 7.072m2.828-9.9a9 9 0 010 12.728M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z"
-              />
-            </svg>
-          </div>
+          <SoundButton
+            soundEnabled={soundEnabled}
+            onClick={() => setSoundEnabled((prev) => !prev)}
+          />
 
           {isAnimationDone && (
             <>
@@ -257,7 +244,12 @@ const Home: NextPage = () => {
         </motion.div>
       </div>
 
-      {isAnimationDone && (!isTodaySpecial ? <TrollFace /> : <Sayings />)}
+      {isAnimationDone &&
+        (!isTodaySpecial ? (
+          <TrollFace soundEnabled={soundEnabled} />
+        ) : (
+          <Sayings soundEnabled={soundEnabled} />
+        ))}
     </div>
   );
 };
